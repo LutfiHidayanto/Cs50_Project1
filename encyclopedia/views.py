3334c77a3f8a006c46dd2709a97bd02f90b42071
programmerltf <lutfihomepc@gmail.com>
2023-02-09 16:42:08 +0700

    adding wiki/title page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 from . import util
+from markdown2 import Markdown
 
 
 def index(request):
@@ -8,3 +8,15 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry(request, title):
+    content = util.get_entry(title)
+    markdowner = Markdown()
+
+    if content == None:
+        return render(request, "encyclopedia\error.html", {"title": title})
+    content = markdowner.convert(content)
+    return render(request, "encyclopedia\entry.html", {
+        "title": title,
+        "content": content
+    })   
