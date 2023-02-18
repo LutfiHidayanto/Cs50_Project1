70e2c756200cb8235bdd1b527b600cc39bafe777
programmerltf <lutfihomepc@gmail.com>
2023-02-18 13:12:32 +0700

    adding search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -20,3 +20,29 @@ def entry(request, title):
         "title": title,
         "content": content
     })   
+
+def search(request):
+    if request.method == "POST":
+        query = request.POST['q']
+        content = util.get_entry(query)
+        markdowner = Markdown()
+
+        if content:
+            content = markdowner.convert(content)
+            return render(request, "encyclopedia\entry.html", {
+                "title": query,
+                "content": content
+            })
+        else:
+            entries = util.list_entries()
+            suggestion = []
+            for entry in entries:
+                if query.lower() in entry.lower():
+                    suggestion.append(entry)
+            return render(request, "encyclopedia\suggestion.html", {
+                "title": query,
+                "content": suggestion
+            })
+
+def newPage(request):
+    pass
